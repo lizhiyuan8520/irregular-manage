4de79692ea51adc7793a1adfb7b771d361ed88ca
LiZhiYuan8520 <1753856457@qq.com>
2024-12-27 17:38:29 +0800

    lcs

--- src/api/lcs.js
+++ src/api/lcs.js
@@ -7,3 +7,24 @@ export const getLcsList = (data) => {
         params: data,
     });
 };
+export const delLcs = (data) => {
+    return request({
+        url: "/intra/monitor/monitorflowing/delete",
+        method: "delete",
+        data,
+    });
+};
+export const getLcsDetailById = (id) => {
+    return request({
+        url: `/intra/monitor/monitorflowing/detail/${id}`,
+        method: "get",
+        params: data,
+    });
+};
+export const saveLcs = (data) => {
+    return request({
+        url: `/intra/monitor/monitorflowing/submit`,
+        method: "post",
+        data,
+    });
+};
